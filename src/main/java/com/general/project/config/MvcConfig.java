185881cf40f2a7d191e77b93b0c0fb9177b5ec69
vladimirshuamev <shumaevwork@gmail.com>
2020-11-14 22:11:49 +0300

    adding feature

--- src/main/java/com/general/project/config/MvcConfig.java
+++ src/main/java/com/general/project/config/MvcConfig.java
@@ -1,12 +1,19 @@
 package com.general.project.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-public class MvcConfig {
+public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+            .addResourceLocations("file:///" + "C:/Users//any/Desktop/idea/CVproject/src/main/resources/static/");
     }
 }
--- src/main/resources/templates/error_404.ftl
+++ src/main/resources/templates/error_404.ftl
@@ -2,5 +2,14 @@
 
 <@l.page>
 
-       <img src="static\mario.jpg" alt="Girl in a jacket">
+<body>
+<p>
+    <img src="/img/404-1-1024x498.jpg" width="600">
+    <img src="/img/travolta.gif">
+</p>
+</body>
+        <#--<div>-->
+            <#--<img src="/img/travolta.gif"  style="margin-left: 600em;">-->
+        <#--</div>-->
+
 </@l.page>
